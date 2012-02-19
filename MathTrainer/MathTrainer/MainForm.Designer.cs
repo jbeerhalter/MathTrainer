82a2100b9ecd09da7e5f029e22f80a28249a06c8
Jonathan Beerhalter <jbeerhalter@gmail.com>
2012-02-19 15:29:20 -0600

    Initial commit for the Math Trainer`

---
+++ MathTrainer/MathTrainer/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MathTrainer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTopNumber = new System.Windows.Forms.Label();
+            this.lblBottomNumber = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblOperand = new System.Windows.Forms.Label();
+            this.lblCorrectIncorrect = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMoneyEarned = new System.Windows.Forms.Label();
+            this.cboFunction = new System.Windows.Forms.ComboBox();
+            this.cboLevel = new System.Windows.Forms.ComboBox();
+            this.cboCrap = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblTopNumber
+            // 
+            this.lblTopNumber.AutoSize = true;
+            this.lblTopNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopNumber.Location = new System.Drawing.Point(164, 88);
+            this.lblTopNumber.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lblTopNumber.Name = "lblTopNumber";
+            this.lblTopNumber.Size = new System.Drawing.Size(292, 55);
+            this.lblTopNumber.TabIndex = 0;
+            this.lblTopNumber.Text = "Top Number";
+            // 
+            // lblBottomNumber
+            // 
+            this.lblBottomNumber.AutoSize = true;
+            this.lblBottomNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBottomNumber.Location = new System.Drawing.Point(164, 143);
+            this.lblBottomNumber.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lblBottomNumber.Name = "lblBottomNumber";
+            this.lblBottomNumber.Size = new System.Drawing.Size(291, 55);
+            this.lblBottomNumber.TabIndex = 1;
+            this.lblBottomNumber.Text = "Bottom Num";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(164, 212);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 55);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(164, 178);
+            this.label3.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(296, 55);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "-----------------";
+            // 
+            // lblOperand
+            // 
+            this.lblOperand.AutoSize = true;
+            this.lblOperand.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperand.Location = new System.Drawing.Point(111, 143);
+            this.lblOperand.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lblOperand.Name = "lblOperand";
+            this.lblOperand.Size = new System.Drawing.Size(52, 55);
+            this.lblOperand.TabIndex = 4;
+            this.lblOperand.Text = "+";
+            // 
+            // lblCorrectIncorrect
+            // 
+            this.lblCorrectIncorrect.AutoSize = true;
+            this.lblCorrectIncorrect.Location = new System.Drawing.Point(177, 273);
+            this.lblCorrectIncorrect.Name = "lblCorrectIncorrect";
+            this.lblCorrectIncorrect.Size = new System.Drawing.Size(122, 37);
+            this.lblCorrectIncorrect.TabIndex = 5;
+            this.lblCorrectIncorrect.Text = "Correct";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(545, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Money Earned";
+            // 
+            // lblMoneyEarned
+            // 
+            this.lblMoneyEarned.AutoSize = true;
+            this.lblMoneyEarned.Location = new System.Drawing.Point(799, 88);
+            this.lblMoneyEarned.Name = "lblMoneyEarned";
+            this.lblMoneyEarned.Size = new System.Drawing.Size(35, 37);
+            this.lblMoneyEarned.TabIndex = 7;
+            this.lblMoneyEarned.Text = "0";
+            // 
+            // cboFunction
+            // 
+            this.cboFunction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFunction.FormattingEnabled = true;
+            this.cboFunction.Items.AddRange(new object[] {
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division"});
+            this.cboFunction.Location = new System.Drawing.Point(100, 12);
+            this.cboFunction.Name = "cboFunction";
+            this.cboFunction.Size = new System.Drawing.Size(183, 45);
+            this.cboFunction.TabIndex = 8;
+            this.cboFunction.SelectedIndexChanged += new System.EventHandler(this.HandleFunctionChanged);
+            this.cboFunction.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HandleFormKeyPress);
+            // 
+            // cboLevel
+            // 
+            this.cboLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboLevel.FormattingEnabled = true;
+            this.cboLevel.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.cboLevel.Location = new System.Drawing.Point(366, 12);
+            this.cboLevel.Name = "cboLevel";
+            this.cboLevel.Size = new System.Drawing.Size(121, 45);
+            this.cboLevel.TabIndex = 9;
+            this.cboLevel.SelectedIndexChanged += new System.EventHandler(this.OnCBOLevelSelectedIndexChanged);
+            this.cboLevel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HandleFormKeyPress);
+            // 
+            // cboCrap
+            // 
+            this.cboCrap.FormattingEnabled = true;
+            this.cboCrap.Location = new System.Drawing.Point(622, 26);
+            this.cboCrap.Name = "cboCrap";
+            this.cboCrap.Size = new System.Drawing.Size(121, 45);
+            this.cboCrap.TabIndex = 10;
+            this.cboCrap.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1684, 1028);
+            this.Controls.Add(this.cboCrap);
+            this.Controls.Add(this.cboLevel);
+            this.Controls.Add(this.cboFunction);
+            this.Controls.Add(this.lblMoneyEarned);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCorrectIncorrect);
+            this.Controls.Add(this.lblOperand);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblBottomNumber);
+            this.Controls.Add(this.lblTopNumber);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "MainForm";
+            this.Text = "Math Trainer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HandleFormKeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTopNumber;
+        private System.Windows.Forms.Label lblBottomNumber;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblOperand;
+        private System.Windows.Forms.Label lblCorrectIncorrect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMoneyEarned;
+        private System.Windows.Forms.ComboBox cboFunction;
+        private System.Windows.Forms.ComboBox cboLevel;
+        private System.Windows.Forms.ComboBox cboCrap;
+    }
+}
+
---
+++ MathTrainer/MathTrainer/MainForm.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MathTrainer
+{
+    public partial class MainForm : Form
+    {
+        private enum FunctionType
+        {
+            Addition = 1,
+            Subtraction = 2,
+            Multiplication = 3,
+            Division = 4
+        }
+        
+        private Func<int,int,int> _currentFunction;
+        private Func<int,int,int> addition = (a,b) => a + b;
+        private Func<int, int, int> subtraction = (a, b) => a - b;
+        private Func<int,int,int> multiplication = (a,b) => a* b;
+        private Func<int, int, int> division = (a, b) => a/b;
+        List<char> _validKeys = new List<char>();
+        List<char> _currentAnswer = new List<char>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetupDefaultProblem();
+            
+            // In the future we'll come up with a mechanism to determine what the current function is
+            _currentFunction = addition;
+        
+            _validKeys.Add("0".ToCharArray()[0]);
+            _validKeys.Add("1".ToCharArray()[0]);
+            _validKeys.Add("2".ToCharArray()[0]);
+            _validKeys.Add("3".ToCharArray()[0]);
+            _validKeys.Add("4".ToCharArray()[0]);
+            _validKeys.Add("5".ToCharArray()[0]);
+            _validKeys.Add("6".ToCharArray()[0]);
+            _validKeys.Add("7".ToCharArray()[0]);
+            _validKeys.Add("8".ToCharArray()[0]);
+            _validKeys.Add("9".ToCharArray()[0]);
+
+            this.lblCorrectIncorrect.Visible = false;
+
+            cboLevel.SelectedIndex = 0;
+
+            cboFunction.DataSource = Enum.GetValues(typeof(FunctionType));
+            cboFunction.SelectedIndex = 0;
+
+            this.KeyDown += new KeyEventHandler(MainForm_KeyDown);
+        }
+
+        void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            cboCrap.Focus();
+            cboCrap.Select();
+        }
+
+        //Sets up a problem on the screen when the app launches
+        private void SetupDefaultProblem()
+        {
+            lblTopNumber.Text = random.Next(1,10).ToString();
+            lblBottomNumber.Text = random.Next(1,10).ToString();
+            lblOperand.Text = "+";
+
+        }
+
+        private void HandleFormKeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Enter is (char)13
+            // Delete is (char)8
+            cboCrap.Focus();
+            cboCrap.Select();
+            char theChar = e.KeyChar;
+
+            if (e.KeyChar == (char)13)
+            {
+                CheckSolution();
+            }
+
+            if (e.KeyChar == (char)8)
+            {
+                if (_currentAnswer.Count > 0)
+                {
+                    _currentAnswer.Remove(_currentAnswer.Last());
+                    UpdateCurrentAnswerLabel();
+                }
+            }
+
+            if (_validKeys.Contains(e.KeyChar))
+            {
+                // YEah, they've pressed a number key!
+                _currentAnswer.Add(e.KeyChar);
+                UpdateCurrentAnswerLabel();
+            }
+        }
+
+        private void CheckSolution()
+        {
+            
+            int curAnswer;
+            if (int.TryParse(new string(
+                _currentAnswer.ToArray()), out curAnswer))
+            {
+                // Write out the current question and answer
+                System.IO.File.AppendAllText("MoneyEarned.txt", DateTime.Now.ToString() +" "+ lblTopNumber.Text + lblOperand.Text + lblBottomNumber.Text + "=" + curAnswer.ToString() + "\r\n");
+ 
+                if (_currentFunction(Convert.ToInt32(lblTopNumber.Text), Convert.ToInt32(lblBottomNumber.Text)) ==
+                    curAnswer)
+                {
+                    ProcessCorrectAnswer();
+                }
+                else
+                {
+                    ProcessIncorrectAnswer();
+                }
+           }
+
+        }
+
+        private void UpdateCurrentAnswerLabel()
+        {
+            lblAnswer.Text = new string(_currentAnswer.ToArray());
+       
+        }
+
+        private void SaveCurrentMoneyEarned()
+        {
+            System.IO.File.AppendAllText("MoneyEarned.txt",DateTime.Now.ToString() + " Lily has earned " + lblMoneyEarned.Text + " so far\r\n");
+
+        }
+        private void ProcessCorrectAnswer()
+        {
+            lblCorrectIncorrect.ForeColor = Color.Green;
+            lblCorrectIncorrect.Text = "Good Job!";
+            lblCorrectIncorrect.Visible = true;
+
+            this.Refresh();
+            System.Threading.Thread.Sleep(1000);
+            lblCorrectIncorrect.Visible = false;
+            lblAnswer.Text = "";
+            _currentAnswer = new List<char>();
+
+            lblMoneyEarned.Text = (Convert.ToDecimal(lblMoneyEarned.Text) + GetPayoutByFunctionTypeAndLevel((FunctionType) cboFunction.SelectedItem,Convert.ToInt32(cboLevel.SelectedItem))).ToString();
+            SaveCurrentMoneyEarned();
+
+            ProcessNextQuestion();
+        }
+
+        private void ProcessIncorrectAnswer()
+        {
+            lblCorrectIncorrect.ForeColor = Color.Red;
+            lblCorrectIncorrect.Text = "Try Again";
+            lblCorrectIncorrect.Visible = true;
+            this.Refresh();
+            System.Threading.Thread.Sleep(1000);
+            lblCorrectIncorrect.Visible = false;
+            lblAnswer.Text = "";
+            _currentAnswer = new List<char>();
+           
+            ProcessNextQuestion();
+        
+        }
+
+        System.Random random = new Random(DateTime.Now.Millisecond);
+        private void ProcessNextQuestion()
+        {
+            int bottom, top;
+            if (cboFunction.SelectedItem == null || cboLevel.SelectedItem == null) return;
+            GetTopAndBottomForFunctionAndLevel(out top, out bottom, (FunctionType)cboFunction.SelectedItem, Convert.ToInt32(cboLevel.SelectedItem));
+            lblTopNumber.Text = top.ToString();
+            lblBottomNumber.Text = bottom.ToString();
+  
+        }
+
+        private void GetTopAndBottomForFunctionAndLevel(out int top, out int bottom, FunctionType funcType, int level)
+        {
+            bottom = 0;
+            top = 0;
+            if (funcType == FunctionType.Addition)
+            {
+                if (level == 1)
+                {
+                    bottom = random.Next(1, 10);
+                    top = random.Next(1, 10);
+                }
+
+                else if (level == 2)
+                {
+                    bottom = 10 * random.Next(1, 10);
+                    top = 10 * random.Next(1, 10);
+                }
+
+                else
+                {
+                    bottom = random.Next(10, 99);
+                    top = random.Next(10, 99);
+                }
+
+            }
+
+            if (funcType == FunctionType.Subtraction)
+            {
+                bottom = random.Next(1, 10);
+                top = random.Next(bottom, 20);
+            }
+
+            if (funcType == FunctionType.Multiplication)
+            {
+                bottom = random.Next(1, 10);
+                top = random.Next(1, 10);
+            }
+
+            if (funcType == FunctionType.Division)
+            {
+                bottom = random.Next(1, 10);
+                top = random.Next(1, 10)*bottom;
+            }
+
+        }
+
+        private decimal GetPayoutByFunctionTypeAndLevel(FunctionType functionType, int level)
+        {
+            if (functionType == FunctionType.Addition)
+            {
+                if (level == 1)
+                {
+                    return .01m;
+                }
+
+                if (level == 2)
+                {
+                    return .015m;
+                }
+
+                if (level == 3)
+                {
+                    return .02m;
+                }
+
+                return .02m;
+
+            }
+
+            if (functionType == FunctionType.Subtraction)
+            {
+                return .01m;
+            }
+
+            if (functionType == FunctionType.Multiplication)
+            {
+                return .02m;
+            }
+
+            if (functionType == FunctionType.Division)
+            {
+                return .02m;
+            }
+
+            return .01m;
+
+        }
+
+        private void HandleFunctionChanged(object sender, EventArgs e)
+        {
+            FunctionType curFunc = (FunctionType) cboFunction.SelectedItem;
+            if (curFunc == FunctionType.Addition)
+            {
+                _currentFunction = addition;
+                lblOperand.Text = "+";
+            }
+
+            if (curFunc == FunctionType.Subtraction)
+            {
+                _currentFunction = subtraction;
+                lblOperand.Text = "-";
+            }
+
+            if (curFunc == FunctionType.Multiplication)
+            {
+                _currentFunction = multiplication;
+                lblOperand.Text = "x";
+            }
+
+            if (curFunc == FunctionType.Division)
+            {
+                _currentFunction = division;
+                lblOperand.Text = "%";
+            }
+            
+            ProcessNextQuestion();
+            cboCrap.Select();
+        }
+
+        private void OnCBOLevelSelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboCrap.Select();
+            ProcessNextQuestion();
+        }
+ 
+    }
+}
